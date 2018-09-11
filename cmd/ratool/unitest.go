ccfdb3e1afbbaec74f53efc3b6aab1826282efe6
Alban Dericbourg <a.dericbourg@criteo.com>
2018-09-11 11:02:11 +0200

    Make samples from test configuration file
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

--- cmd/ratool/unitest.go
+++ cmd/ratool/unitest.go
@@ -5,8 +5,11 @@ import (
 	"github.com/criteo/graphite-remote-adapter/client/graphite/paths"
 	"github.com/criteo/graphite-remote-adapter/config"
 	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/common/model"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"strings"
 )
 
 const (
@@ -14,9 +17,7 @@ const (
 )
 
 type unittestCmd struct {
-	inputMetricsFile string
-	configFile       string
-	clientType       string
+	inputConfigFile string
 }
 
 func configureUnittestCmd(app *kingpin.Application) {
@@ -25,33 +26,37 @@ func configureUnittestCmd(app *kingpin.Application) {
 		unittestCmd = app.Command("unittest", unittestHelp)
 	)
 
-	addMetricsFileFlag(unittestCmd, &w.inputMetricsFile)
-
-	unittestCmd.Flag("config.file", "Graphite-remote-adapter configuration file path.").
-		Required().ExistingFileVar(&w.configFile)
-
-	unittestCmd.Flag("client", "Graphite-remote-adapter client to use.").
-		Default("graphite").EnumVar(&w.clientType, "graphite")
+	unittestCmd.Flag("test.file", "Unit-test description file.").
+		Required().ExistingFileVar(&w.inputConfigFile)
 
 	unittestCmd.Action(w.Unittest)
 }
 
 func (w *unittestCmd) Unittest(ctx *kingpin.ParseContext) error {
 	setupLogger()
-	fileCfg, err := config.LoadFile(logger, w.configFile)
+
+	testCfg, err := loadUnittestConfig(w.inputConfigFile)
 	if err != nil {
-		level.Error(logger).Log("err", err, "msg", "Error loading config file")
+		level.Error(logger).Log("err", err, "msg", "error loading unit-test description file")
 		return err
 	}
 
-	samples, err := loadSamplesFile(w.inputMetricsFile)
+	graCfg, err := config.LoadFile(logger, testCfg.ConfigFile)
 	if err != nil {
+		level.Error(logger).Log("err", err, "msg", "error loading remote-adapter configuration file")
 		return err
 	}
 
-	if w.clientType == "graphite" {
+	fmt.Printf("# Testing %s\n", testCfg.ConfigFile)
+	for _, testContext := range testCfg.Tests {
+		fmt.Printf("## %s\n", testContext.Name)
+		samples, err := makeSamples(testContext.Input)
+		if err != nil {
+			return err
+		}
+
 		for _, s := range samples {
-			datapoints, _ := paths.ToDatapoints(s, paths.FormatCarbon, "", fileCfg.Graphite.Write.Rules, fileCfg.Graphite.Write.TemplateData)
+			datapoints, _ := paths.ToDatapoints(s, paths.FormatCarbon, "", graCfg.Graphite.Write.Rules, graCfg.Graphite.Write.TemplateData)
 			for _, dt := range datapoints {
 				fmt.Print(dt)
 			}
@@ -59,6 +64,11 @@ func (w *unittestCmd) Unittest(ctx *kingpin.ParseContext) error {
 	}
 
 	return nil
+}
+
+func makeSamples(input string) ([]*model.Sample, error) {
+	reader := strings.NewReader(input)
+	return readSamplesFile(reader)
 }
 
 type unittestConfig struct {
@@ -70,6 +80,19 @@ type testConfig struct {
 	Name   string `yaml:"name"`
 	Input  string `yaml:"input"`
 	Output string `yaml:"output"`
+}
+
+func loadUnittestConfig(filePath string) (*unittestConfig, error) {
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	cfg, err := parseUnittestConfig(content)
+	if err != nil {
+
+	}
+
+	return cfg, nil
 }
 
 func parseUnittestConfig(content []byte) (*unittestConfig, error) {
